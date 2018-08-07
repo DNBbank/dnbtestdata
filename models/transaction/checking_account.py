9c169b397147dda4339b6b3213650f8077fa2eb1
Stian Gropen Søgård <stiangs@Stians-MacBook-Pro.local>
2018-08-07 18:32:36 +0200

    Add CheckingAccount and Transaction

---
+++ models/transaction/checking_account.py
@@ -0,0 +1,14 @@
+'''
+Creates transactions for a checking account (BRUKSKONTO)
+
+Based on the transaction script written by Severin Sjømark
+'''
+
+import datetime, math, random
+from datetime import timdedelta, date
+
+import numpy as np
+import pandas as pd
+
+class CheckingAccount:
+    pass
