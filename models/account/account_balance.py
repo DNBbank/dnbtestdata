fed86662eac3bcdee182ccc74c5efc45eba2ae51
Andrew Ginns <ginns.aw@gmail.com>
2018-10-22 14:17:03 +0200

    Added missing fields to account_balance

--- models/account/account_balance.py
+++ models/account/account_balance.py
@@ -2,11 +2,12 @@ class AccountBalance:
     @classmethod
     def generate_random_account_balance_json(cls, account_number, balance):
         account_balance = {
-            'bookedBalance': balance,
+            'availableBalanceWithCredit': balance,
             'availableBalanceWithoutCredit': balance,
+            'bookedBalance': balance,
+            'accountNumber': account_number,
             'accountCurrency': 'NOK',
-            'creditLimit': 0,
-            'accountNumber': account_number
+            'creditLimit': 0
         }
 
         return account_balance
